e034130ce3458fea5dec57527841158eea8b75ed
NirmalPrabhuS <nirmal.unique@gmail.com>
2021-07-17 19:52:57 +0530

    Implemented JWT for authentication

--- models/User.js
+++ models/User.js
@@ -1,24 +1,41 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+
 const UserSchema = new mongoose.Schema({
         name:{
             type:String,
+            //required:[true,"enter name"]
             required:true
         },
         email:{
             type:String,
+            //required:[true,"enter email"],
             required:true,
             unique:true
         },
         password:{
             type:String,
             required:true
+            //required:[true,"enter password"]
         },
         date:{
             type:Date,
             default:Date.now()
         }
-    });
+    }
+    //, {typeKey:'$type'}
+    );
+
+    UserSchema.pre("save", async function(next){
+
+        const user=this
+
+        if(user.isModified("password")){
+            user.password= await bcrypt.hash(user.password,10);
+        }
+        next();
+    })
 
     module.exports = User = mongoose.model('user',UserSchema);
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,14 +2,82 @@ const express = require('express');
 
 const router = express.Router();
 
-//@route  POST api/user
+const {check,validationResult} = require('express-validator')
+
+const User = require('../../models/User');
+
+const bcyrpt = require('bcryptjs');
+
+const jwt =require('jsonwebtoken');
+
+const config= require('config');
+
+//@route  POST api/users
 //@access public 
 // @desc   Register user
-router.post('/',(req,res)=>
-{
-    console.log(req.body);
+router.post('/',
+[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please enter valid email').isEmail(),
+    check('password','Please enter 6 or more character').isLength({min:6})
+],
+async (req,res)=> {  
+    const error = validationResult(req);
+
+    if(!error.isEmpty()){
+        return res.status(400).json({errors:error.array()});
+    }
     
-    res.send('User Route')
+    const { name,email,password } = req.body;
+
+    try{
+    
+        //see if user exist
+
+    let user = await User.findOne({email});
+
+        if(user)
+        {
+          return res.status(400).json({ errors: [ {msg:' User alreay exist'} ] });
+        }
+           
+        
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        //encyrpt password
+        const salt =   await bcyrpt.genSalt(10);
+
+        user.password =  await bcyrpt.hash(password,salt)
+
+        // inserting user to MongoDB
+        await user.save();
+
+        
+        //returning json web token
+        const payload = {
+            user:{id:user.id}
+        };
+
+        jwt.sign(
+            payload,
+            config.get('SecretKey'),
+            {expiresIn:360000},
+            (err,token)=> {
+                if(err) throw err;
+                res.json({token});
+            }
+        );
+    }
+    catch(error)
+    {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+   
 });
 
 module.exports=router;
